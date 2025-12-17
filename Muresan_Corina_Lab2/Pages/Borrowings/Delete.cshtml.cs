0b00b60818484eec96309613c7f9f0074b4d5f93
Corina Muresan <corina_emi2008@yahoo.com>
2025-12-17 12:17:39 -0800

    fixes- borrowings details and delete

---
+++ Muresan_Corina_Lab2/Pages/Borrowings/Delete.cshtml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Muresan_Corina_Lab2.Data;
+using Muresan_Corina_Lab2.Models;
+
+namespace Muresan_Corina_Lab2.Pages.Borrowings
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Muresan_Corina_Lab2.Data.Muresan_Corina_Lab2Context _context;
+
+        public DeleteModel(Muresan_Corina_Lab2.Data.Muresan_Corina_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Borrowing Borrowing { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var borrowing = await _context.Borrowing
+             .Include(b => b.Member)
+             .Include(b => b.Book)
+             .ThenInclude(b => b.Author)
+             .FirstOrDefaultAsync(m => m.ID == id);
+
+
+            if (borrowing is not null)
+            {
+                Borrowing = borrowing;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var borrowing = await _context.Borrowing.FindAsync(id);
+            if (borrowing != null)
+            {
+                Borrowing = borrowing;
+                _context.Borrowing.Remove(Borrowing);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Muresan_Corina_Lab2/Pages/Borrowings/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Muresan_Corina_Lab2.Data;
+using Muresan_Corina_Lab2.Models;
+
+namespace Muresan_Corina_Lab2.Pages.Borrowings
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Muresan_Corina_Lab2.Data.Muresan_Corina_Lab2Context _context;
+
+        public IndexModel(Muresan_Corina_Lab2.Data.Muresan_Corina_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Borrowing> Borrowing { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Borrowing = await _context.Borrowing
+            .Include(b => b.Book)
+            .ThenInclude(b => b.Author)
+            .Include(b => b.Member)
+            .ToListAsync();
+
+        }
+    }
+}
